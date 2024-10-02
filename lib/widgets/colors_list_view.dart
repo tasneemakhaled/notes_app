e485a2b1c6947b5b24c5bc2b15900beab50c040e
tasneemkhaled <khaledtasneem402@gmail.com>
2024-10-02 23:49:00 +0200

    create list of colors part one

---
+++ lib/widgets/colors_list_view.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:notes_app/widgets/color_item.dart';
+
+class ColorsListView extends StatelessWidget {
+  const ColorsListView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 60,
+      child: ListView.builder(
+          scrollDirection: Axis.horizontal,
+          itemCount: 10,
+          itemBuilder: (context, index) {
+            return ColorItem();
+          }),
+    );
+  }
+}
